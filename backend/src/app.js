d71d2c2cefd00a00572ef8e2d4de5da5d19238b3
DhruvNaina <dhruv4857.be21@chitkara.edu.in>
2026-06-09 19:52:33 +0530

    Fix production API URLs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,18 @@
 import express from "express";
 import morgan from "morgan";
-import authRouter from "./routes/authRouter.js";
-import todosrouter from "./routes/todoroutes.js";
 import cors from "cors";
 
-
+import authRouter from "./routes/authRouter.js";
+import todosrouter from "./routes/todoroutes.js";
 
 const app = express();
 
 app.use(cors());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
 
 app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/auth", authRouter);
-app.use("/set/todo" , todosrouter)
+app.use("/api/todo", todosrouter);
 
 export default app;
